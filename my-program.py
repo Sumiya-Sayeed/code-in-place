d5b112b295d61e68a8986ca4f470d2e51e999967
Sumiya-Sayeed <sumiyasayeed@gmail.com>
2024-06-27 20:38:06 +0600

    first commit

---
+++ my-program.py
@@ -0,0 +1,8 @@
+MAX_NUMBER = 50
+
+def even():
+  for i in range(MAX_NUMBER):
+    if i % 2 == 0:
+      print(i)
+
+even()
---
+++ something-else.py
@@ -0,0 +1,5 @@
+def convert(temp_in_celsius):
+  temp_in_far = temp_in_celsius * 1.8 + 32
+  return temp_in_far
+
+print(convert(20))
